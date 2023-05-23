b9c483616fe8add80a59e62d80b7ba5a154df9a9
Eileenj57 <jovenineileen@gmail.com>
2023-05-23 18:09:56 +0200

    post.model.ts

---
+++ src/app/models/post.model.ts
@@ -0,0 +1,10 @@
+export class Post {
+    title: string;
+    content: string;
+  
+    constructor(title: string, content: string) {
+      this.title = title;
+      this.content = content;
+    }
+  }
+  
