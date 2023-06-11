429ec1685d8295c8e819518364bbcd18a4de90ca
Raphael PERRIN <raphael.perrin754@gmail.com>
2023-06-11 22:36:17 +0200

    Now able to click on TOP 3 trends activities

--- src/app/pages/home/components/top-activity/top-activity.component.ts
+++ src/app/pages/home/components/top-activity/top-activity.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ActivityDTO} from "../../../../posts/models/activity-dto";
+import {ActivityComponent} from "../../../activity/components/activity.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-top-activity',
@@ -8,9 +10,16 @@ import {ActivityDTO} from "../../../../posts/models/activity-dto";
 })
 export class TopActivityComponent implements OnInit{
 
+  constructor(private router: Router) {
+  }
+
   @Input() activityDTO!: ActivityDTO;
 
   ngOnInit(): void {
+  }
+
+  onActivityClicked(): void {
+    this.router.navigateByUrl(`activity/${this.activityDTO.id}`)
   }
 
 }
