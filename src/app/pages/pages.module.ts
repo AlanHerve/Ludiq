b352171450caa24802464f46afc6d3fd9eaf2f81
Raphael PERRIN <raphael.perrin754@gmail.com>
2023-06-13 15:27:32 +0200

    Profile modification bugued & beginning of post page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {CoreModule} from "../core/core.module";
 import { NotificationsComponent } from './notifications/components/notifications.component';
 import {TopBarComponent} from "../shared/components/top-bar/top-bar.component";
+import { DetailedPostComponent } from './post/components/detailed-post/detailed-post.component';
 
 @NgModule({
   declarations: [
     NotificationsComponent,
-    TopBarComponent
+    TopBarComponent,
+    DetailedPostComponent
   ],
   exports: [
     CoreModule,
