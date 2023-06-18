fdc5930962da493bbc6d309f2ceabb42f006fe25
Raphael PERRIN <raphael.perrin754@gmail.com>
2023-06-18 16:00:52 +0200

    Deleted a file

--- Backend/EntryPoint/friend.php
+++ Backend/EntryPoint/friend.php
@@ -5,8 +5,6 @@ header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Origin,Content-Type');
 
 include("../Repositories/FriendRepository.php");
-include("../DTOs/HobbyPostDTO.php");
-
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $friendRepository = FriendRepository::getInstance();
