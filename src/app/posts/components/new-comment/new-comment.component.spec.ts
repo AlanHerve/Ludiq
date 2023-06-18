580f4bab1f59636e0dce44a1cae11011a006237c
Eileenj57 <jovenineileen@gmail.com>
2023-06-18 20:39:25 +0200

    commenting the code

--- src/app/posts/components/new-comment/new-comment.component.spec.ts
+++ src/app/posts/components/new-comment/new-comment.component.spec.ts
@@ -1,20 +1,26 @@
+// Import necessary modules
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NewCommentComponent } from './new-comment.component';
 
+// Unit test for NewCommentComponent
 describe('CommentComponent', () => {
   let component: NewCommentComponent;
   let fixture: ComponentFixture<NewCommentComponent>;
 
   beforeEach(() => {
+    // Set up TestBed
     TestBed.configureTestingModule({
       declarations: [NewCommentComponent]
     });
+    // Create a fixture for the component
     fixture = TestBed.createComponent(NewCommentComponent);
+    // Get an instance of the component
     component = fixture.componentInstance;
+    // Trigger change detection for the component
     fixture.detectChanges();
   });
 
+  // A simple test to verify component creation
   it('should create', () => {
     expect(component).toBeTruthy();
   });
--- src/app/posts/components/new-comment/new-comment.component.ts
+++ src/app/posts/components/new-comment/new-comment.component.ts
@@ -1,3 +1,4 @@
+// Import necessary modules and interfaces
 import {Component, Input, OnInit} from '@angular/core';
 import {PostDTO} from "../../models/post-dto";
 import {UserService} from "../../../services/user.service";
@@ -5,6 +6,10 @@ import {PostService} from "../../services/post.service";
 import {CommentDTO} from "../../models/comment-dto";
 import {UserDTO} from "../../../models/user-dto";
 
+/**
+ * NewCommentComponent is a component to create new comments.
+ * It is used to handle all the functionalities for creating a new comment.
+ */
 @Component({
   selector: 'app-new-comment',
   templateUrl: './new-comment.component.html',
@@ -17,11 +22,14 @@ export class NewCommentComponent implements OnInit {
   protected comment!: string;
   protected commentDTO!: CommentDTO;
 
+  // Constructor for the component. It injects two services: UserService and PostService
   constructor(private userService: UserService,
               private postsService: PostService) {
   }
 
+  // ngOnInit method gets called once, immediately after the default constructor, when Angular finishes initializing the component
   ngOnInit(): void {
+    // Initialize commentDTO
     this.commentDTO = {
       id: -1,
       userDTO: new UserDTO(-1, '', ''),
@@ -30,6 +38,7 @@ export class NewCommentComponent implements OnInit {
       time: '',
     };
 
+    // Get the current user
     this.userService.findUserById(this.userService.getCurrentId()).subscribe({
       next: (user) => {
         this.commentDTO.userDTO = user;
@@ -40,15 +49,17 @@ export class NewCommentComponent implements OnInit {
     })
   }
 
+  // Method to add a new comment
   addComment() {
-
     this.commentDTO.content = this.comment;
 
+    // Call the addComment method from the PostService
     this.postsService.addComment(this.commentDTO).subscribe(response => {
       if (response) {
+        // Reset the comment
         this.comment = '';
       } else {
-        // Gére l'erreur
+        // Handle the error
         console.error('Erreur lors de l\'ajout du commentaire');
       }
     });
