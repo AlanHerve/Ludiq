173c84b931fab25e7b1a8c37f093d8c8e34eff28
Eileenj57 <jovenineileen@gmail.com>
2023-06-18 19:48:48 +0200

    commenting the code

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,17 @@
+// Import the required Angular modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+/**
+ * The AuthModule is a feature module that organizes the code related
+ * to the authentication features of your application.
+ */
 @NgModule({
   declarations: [
   ],
   imports: [
+    // Import required modules for this module
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
--- src/app/ludiq-forms/components/form-modify-profile/form-modify-profile.module.ts
+++ src/app/ludiq-forms/components/form-modify-profile/form-modify-profile.module.ts
@@ -1,8 +1,13 @@
+// Import necessary modules
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
 import {FormModifyProfileComponent} from "./form-modify-profile.component";
 
+/**
+ * The FormModifyProfileModule is a feature module that organizes
+ * the code related to the form modification feature of your application.
+ */
 @NgModule({
   declarations: [],
   imports:[
