78b6538180bbd8e1add751468fd92fc1f9bbb20f
Eileenj57 <jovenineileen@gmail.com>
2023-06-18 19:42:08 +0200

    commenting code

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,32 +1,40 @@
+// Import necessary libraries
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserService } from '../services/user.service';
 
+// Uses the Injectable decorator to allow dependency injection into this class
 @Injectable({
   providedIn: 'root'
+  // Specifies this class is a singleton and can be injected anywhere in the application
 })
+
+/**
+ * AuthGuard is used to protect routes and control access to users based on certain conditions.
+ * It uses the UserService to check if a user is logged in. If not, the user is redirected to '/hub'
+ * and the canActivate method returns 'false', blocking access to the route. If the user is logged in,
+ * the canActivate method returns 'true', allowing access to the route.
+ */
 export class AuthGuard {
 
   constructor(private authService: UserService, private router: Router) {}
-
+  /**
+   * Method called by Angular to decide whether a route can be activated or not.
+   * It can return an Observable, a Promise or a boolean value.
+   * If the user is not logged in, it redirects to '/hub' route and returns 'false'.
+   * If the user is logged in, it returns 'true'.
+   */
   canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    // If the user is not logged in...
     if (!this.authService.isLoggedIn()) {
+      // ...redirects to '/hub' route
       this.router.navigate(['/hub']);
+      // ...and returns 'false' which means the route cannot be activated
       return false;
     }
+    // If the user is logged in, return 'true' which means the route can be activated
     return true;
   }
 
-  canActivateOrganization(): Observable<boolean> | Promise<boolean> | boolean {
-    if (!this.authService.isLoggedIn() && !this.authService.isPartOfOrganization(2)){
-      this.router.navigate(['/hub']);
-      return false;
-    }else if(this.authService.isLoggedIn()) {
-      this.router.navigate(['/home']);
-      return false;
-    }
-    return true;
-
-  }
 }
