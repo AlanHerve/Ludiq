a8366feea54ea38ae17ab16a8e5d8b46ec00f11a
Eileenj57 <jovenineileen@gmail.com>
2023-06-18 22:19:20 +0200

    Update friend-request-dto.ts

--- src/app/models/friend-request-dto.ts
+++ src/app/models/friend-request-dto.ts
@@ -2,7 +2,7 @@ import {UserDTO} from "./user-dto";
 
 /**
  * Information on friendship status between two users, relationship
- * status: friendship accepted or still waitig
+ * status: friendship accepted or still waiting
  */
 export class FriendRequestDTO{
   constructor(public user: UserDTO, public status: number, public requester: number) {
