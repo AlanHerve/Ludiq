f0e861b0e9e961c82d30d7d1f2c19cd8e2b33b99
Malsherry <92732205+Malsherry@users.noreply.github.com>
2023-06-18 21:17:39 +0200

    comments

--- Backend/EntryPoint/comment.php
+++ Backend/EntryPoint/comment.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Origin,Content-Type');
 
 require_once '../Repositories/CommentRepository.php';
-
+//if the user wants to post or delete a comment
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $body = file_get_contents('php://input');
   $data = json_decode($body, true);
@@ -20,12 +20,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
 
 }
-
+// if we need to get the comments
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  // create an instance of CommentRepository
   $commentRepository = CommentRepository::getInstance();
+  // we can either get all comments of a post
   if ($_GET['type'] == "all_comments") {
     echo json_encode($commentRepository->getAllComments($_GET['postID']));
   }
+  // or only get three
   if ($_GET['type'] == "three_comments") {
     echo json_encode($commentRepository->getThreeComments($_GET['postID']));
   }
