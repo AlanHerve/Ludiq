96bce6003582276a9385d6fe428fea7f0b0ef301
Malsherry <92732205+Malsherry@users.noreply.github.com>
2023-06-18 21:09:31 +0200

    comments

--- Backend/EntryPoint/comment.php
+++ Backend/EntryPoint/comment.php
@@ -9,7 +9,9 @@ require_once '../Repositories/CommentRepository.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $body = file_get_contents('php://input');
   $data = json_decode($body, true);
+  // get an instance of CommentRepository
   $commentRepository = CommentRepository::getInstance();
+  // depending on the 'type' property, we either add a comment or delete one, passing the necessary data to methods in commentRepository
   switch ($data['type']){
     case "addComment": echo json_encode($commentRepository->addComment($data['comment']['userDTO'], $data['comment']['content'], $data['comment']['postID']));
       break;
